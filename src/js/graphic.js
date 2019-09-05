50cab9a266af57b2abb03464cece5e11913161c8
Jan Diehm <jadiehm@gmail.com>
2019-09-05 18:38:28 -0500

    tests out adding paths and dots to map

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -5,6 +5,29 @@ import './pudding-chart/count-table'
 let pubsMap;
 let pubCountsData = null;
 let pubCountsTable = null;
+let coordinates = [
+	[-122.48369693756104, 37.83381888486939],
+	[-122.48348236083984, 37.83317489144141],
+	[-122.48339653015138, 37.83270036637107],
+	[-122.48356819152832, 37.832056363179625],
+	[-122.48404026031496, 37.83114119107971],
+	[-122.48404026031496, 37.83049717427869],
+	[-122.48348236083984, 37.829920943955045],
+	[-122.48356819152832, 37.82954808664175],
+	[-122.48507022857666, 37.82944639795659],
+	[-122.48610019683838, 37.82880236636284],
+	[-122.48695850372314, 37.82931081282506],
+	[-122.48700141906738, 37.83080223556934],
+	[-122.48751640319824, 37.83168351665737],
+	[-122.48803138732912, 37.832158048267786],
+	[-122.48888969421387, 37.83297152392784],
+	[-122.48987674713133, 37.83263257682617],
+	[-122.49043464660643, 37.832937629287755],
+	[-122.49125003814696, 37.832429207817725],
+	[-122.49163627624512, 37.832564787218985],
+	[-122.49223709106445, 37.83337825839438],
+	[-122.49378204345702, 37.83368330777276]
+]
 
 const $seeMoreButtonColor = d3.select('#color-noun button')
 const $seeMoreButtonRoyalty = d3.select('#royalty-noun button')
@@ -79,7 +102,69 @@ function buildMap() {
 
 	pubsMap.scrollZoom.disable();
 	pubsMap.addControl(new mapboxgl.NavigationControl());
+
+	pubsMap.on('load', function(){
+		addRoute(coordinates)
+		addPubPoints(coordinates)
+	})
 }
+
+function addPubPoints(coordinates) {
+	pubsMap.addLayer({
+		'id': 'pubs',
+		'type': 'circle',
+		'source': {
+			'type': 'geojson',
+			'data': {
+				'type': 'FeatureCollection',
+				'features': [{
+					'geometry': {
+						'type': 'Point',
+						'coordinates': coordinates[0]
+					},
+					'properties': {
+						'title': 'Mapbox DC'
+					}
+				}]
+			}
+		},
+		'paint': {
+			'circle-radius': 6,
+			'circle-color': '#C80E0E',
+			'circle-stroke-width': 1,
+    	'circle-stroke-color': '#000'
+		}
+	})
+}
+
+function addRoute(coordinates){
+	console.log(coordinates)
+
+	pubsMap.addLayer({
+		'id': 'route',
+		'type': 'line',
+		'source': {
+			'type': 'geojson',
+			'data': {
+				'type': 'Feature',
+				'properties':{},
+				'geometry': {
+					'type': 'LineString',
+					'coordinates': coordinates
+				}
+			}
+		},
+		'layout': {
+			'line-join': 'round',
+			'line-cap': 'round'
+		},
+		'paint': {
+			'line-color': '#C80E0E',
+			'line-width': 8
+		}
+	})
+}
+
 function resize() {}
 
 function init() {
